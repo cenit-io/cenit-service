36dd68620bb31e88af536683619a6e89d07e1cb4
macarci <macarci@gmail.com>
2016-09-19 12:38:02 -0400

    Protect from forgery with null session

--- app/controllers/cenit/service/application_controller.rb
+++ app/controllers/cenit/service/application_controller.rb
@@ -1,7 +1,7 @@
 module Cenit
   module Service
     class ApplicationController < ActionController::Base
-      protect_from_forgery with: :exception
+      protect_from_forgery with: :null_session
 
     end
   end
